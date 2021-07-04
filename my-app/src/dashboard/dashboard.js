401c91b6d1d30e26863f2924d85503c1d91bb2a0
NaidaFatic <faticnaida@gmail.com>
2021-07-04 21:11:15 +0200

    dashboard

--- my-app/src/dashboard/dashboard.js
+++ my-app/src/dashboard/dashboard.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import './dashboard.css';
 
 export default function Dashboard() {
   return(
-    <h2>Dashboard</h2>
+    <div className="dash">
+    <div className="profile-left">
+      <div className="profile-picture-left"></div>
+      <div className="company-name">Company name</div>
+      <div className="saved-posts">
+        <div className="saved-name">Saved Posts</div>
+        <div className="saved-name">Saved Post1</div>
+      </div>
+    </div>
+    <div id="dash-box">
+      <div className="post">
+        <div className="post-desc">
+        <div className="name">Title</div>
+        <div className="description">Description</div>
+        </div>
+        <div className="post-image"></div>
+        <div className="price">3$</div>
+      </div>
+      <div className="left">
+        <div className="profile-picture"></div>
+        <div className="message"></div>
+        <div className="phone"></div>
+      </div>
+    </div>
+    </div>
   );
 }
