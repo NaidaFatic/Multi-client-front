7b85c8b13aa9c2d25dcaea204f140c73cb0bfdcc
NaidaFatic <faticnaida@gmail.com>
2021-07-05 17:43:35 +0200

    map fixed

--- my-app/src/dashboard/dashboard.js
+++ my-app/src/dashboard/dashboard.js
@@ -4,51 +4,45 @@ import './dashboard.css';
 import fetchData from "../utils/ajax";
 import decoded from "../utils/decodeToken";
 
-const useFetch = () => {
-    const [user, setUser] = useState(null);
-    const [article, setArticle] = useState(null);
-    const [articles, setArticles] = useState(null);
 
-    useEffect(()=>{
-        async function getData(){
-            let decode = await decoded();
-            let result = await fetchData("users/"+decode.uid, "GET")
-            let retrieved_user = result.data.company_name;
-            setUser(retrieved_user);
+class Dashboard extends React.Component{
+    state = {
+        user:null,
+        article:null,
+        posts: []
+    }
 
-            let result_article = await fetchData("articles/"+result.data.saved_posts, "GET")
-            let retrieved_article = result_article.data;
-            setArticle(retrieved_article.name);
-
-            let result_articles = await fetchData("articles/", "GET")
-            let retrieved_articles = result_articles.data;
-            setArticles(retrieved_articles);
-        }
-        getData();
-    },[1])
-    return {user, article, articles}
+async componentDidMount() {
+    let decode = await decoded();
+    let result = await fetchData("users/"+decode.uid, "GET")
+    let retrieved_user = result.data;
+    let result_article = await fetchData("articles/"+result.data.saved_posts, "GET")
+    let retrieved_article = result_article.data;
+    let result_articles = await fetchData("articles/", "GET")
+    let retrieved_articles = result_articles.data;
+    this.setState({user:retrieved_user.company_name ,article:retrieved_article.name, posts: retrieved_articles}  )
 }
 
-export default function Dashboard() {
-  const user  = useFetch();
-  const article = useFetch();
-  var articles = useFetch();
-  console.log(Array.isArray(articles.articles));
-  console.log(articles.articles);
-  articles.articles.map((item, i) => (console.log(item)));
+
+ render(){
+  console.log(this.state.posts);
+  this.state.posts.map((item, i) => (console.log(item)));
   return(
     <div className="dash">
     <div className="profile-left">
       <div className="profile-picture-left"></div>
-      <div className="company-name">{user.user}</div>
+      <div className="company-name">{this.state.user}</div>
       <div className="saved-posts">
         <div className="saved-name">Saved Posts</div>
-        <div className="saved-name">{article.article}</div>
+        <div className="saved-name">{this.state.article}</div>
       </div>
     </div>
-    <div className="posts">{articles.articles.map((item, i) => (
-    <Post name={item.name} desc={item.description} price={item.price} /> ))}
+    <div className="posts">{this.state.posts.map((item, i) => (
+    <Post key={item._id} name={item.name} desc={item.description} price={item.price} /> ))}
     </div>
     </div>
   );
+ }
 }
+
+export default Dashboard;
